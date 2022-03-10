f1bce30ddafc4b0bf0822c353463d720538dea65
uzunhair <uzunhair@yandex.ru>
2022-03-10 10:08:42 +0300

    Update #2022

--- src/js/setting.js
+++ src/js/setting.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 
 // $(document).ready(() => {
 //   $(() => {
@@ -10,3 +10,24 @@ import $ from 'jquery';
 //     $('[data-toggle="popover"]').popover();
 //   });
 // });
+
+
+// core version + navigation, pagination modules:
+import Swiper, { Pagination }  from 'swiper';
+// import Swiper and modules styles
+// import 'swiper/css';
+
+// init Swiper:
+const swiper = new Swiper(".mySwiper", {
+  modules: [Pagination],
+  cssMode: true,
+  mousewheel: true,
+  keyboard: true,
+  slidesPerView: 1,
+  spaceBetween: 12,
+  // loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
